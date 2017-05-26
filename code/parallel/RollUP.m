5c03a4695ff3c77e387599d8b9d5e49397b84b8f
DeanZeng <zfangdi@gmail.com>
2017-05-26 19:13:15 +0800

    modify structure of input and output

--- code/parallel/RollUP.m
+++ code/parallel/RollUP.m
@@ -1,29 +1,33 @@
 %% rolling calculation per week
-RollNum=168;         %%滚动次数
+% addpath C:\gurobi702\win64\matlab;
+% warning( 'off', 'MATLAB:lang:badlyScopedReturnValue' );
+RollNum=364;         %%滚动次数
 RollStart=(1:24:8737)';       %%滚动周期起始时间
 Iter=zeros(RollNum,1);        %%迭代次数
 %% full period input data
 in_full = load('input_data.mat', 'in');
 %%------------------------ initialization ---------------------------------
 in_full = in_full.in;
-in    = in_full;
+in_area = in_full.area;
 %%------------ system data
-in.T=0;                      %% time horizons
-in.TD=0;                     %% number of days
-T=in.T; A=in.A; TD=in.TD;
+A=in_full.A;               %% number of areas
+T=8736;                   %% time horizons
+TD=0;                     %% number of days
 %%------------- area data
-for a=1:in.A
-    in.area(a).Onoff_t0=zeros(1,in.area(a).Nunit);       %% initial status at t=0
-    in.area(a).Pthermal_t0=zeros(1,in.area(a).Nunit);    %% initial output at t=0
-    in.area(a).On_t0=zeros(1,in.area(a).Nunit);          %% length of time unit g has to be on at the beginning
-    in.area(a).Off_t0=zeros(1,in.area(a).Nunit);         %% length of time unit g has to be off at the beginning
-    in.area(a).Demand=[];        %% demand
-    in.area(a).Windmax=[];      %% theory output of wind power
-    in.area(a).PVmax  = [];      %% theory output of PV
-    in.area(a).Ftie0=[];          %% fixed power flow
-    in.area(a).Etie =[];          %% exchage energy each day
-    in.area(a).ReserveUp=[];      %% up reserve
-    in.area(a).ReserveDn=[];      %% down reserve
+for a=1:A
+    in_area(a).T=0;
+    in_area(a).TD=0;
+%     in_area(a).Onoff_t0=zeros(1,in_area(a).Nunit);       %% initial status at t=0
+%     in_area(a).Pthermal_t0=zeros(1,in_area(a).Nunit);    %% initial output at t=0
+%     in_area(a).On_t0=zeros(1,in_area(a).Nunit);          %% length of time unit g has to be on at the beginning
+%     in_area(a).Off_t0=zeros(1,in_area(a).Nunit);         %% length of time unit g has to be off at the beginning
+    in_area(a).Demand=[];        %% demand
+    in_area(a).Windmax=[];      %% theory output of wind power
+    in_area(a).PVmax  = [];      %% theory output of PV
+    in_area(a).Ftie0=[];          %% fixed power flow
+    in_area(a).Etie =[];          %% exchage energy each day
+    in_area(a).ReserveUp=[];      %% up reserve
+    in_area(a).ReserveDn=[];      %% down reserve
 end
 %%-------------- results
 Pwind_F=zeros(T,A);    %% output of wind power 
@@ -35,31 +39,31 @@ shutdown_F=cell(1,A);            %% shut down indicator
 Ftie_F=cell(1,A);
 
 %% roll up
-poll=parpool;
+% pool=parpool;
 tic;
 for roll=1:RollNum
     %%input the rth rolling period data
-    for a=1:A
-        in.T=RollStart(roll+1)-RollStart(roll);
+    parfor a=1:A
+        in_area(a).T=RollStart(roll+1)-RollStart(roll);
         if roll==1
-            in.area(a).Onoff_t0=in_full.area(a).Onoff_t0;       
-            in.area(a).Pthermal_t0=in_full.area(a).Pthermal_t0;   
-            in.area(a).On_t0=in_full.area(a).On_t0;         
-            in.area(a).Off_t0=in_full.area(a).Off_t0;       
+%             in_area(a).Onoff_t0=in_full.area(a).Onoff_t0;       
+%             in_area(a).Pthermal_t0=in_full.area(a).Pthermal_t0;   
+%             in_area(a).On_t0=in_full.area(a).On_t0;         
+%             in_area(a).Off_t0=in_full.area(a).Off_t0;       
         else
-            in.area(a).Onoff_t0=out.area(a).onoff(in.T,:);      
-            in.area(a).Pthermal_t0=out.area(a).Pthermal(in.T);    
-            [in.area(a).On_t0,in.area(a).Off_t0] = getOnoff_t0(out.area(a).onoff,in.area(a).Minup,in.area(a).Mindown);       
+            in_area(a).Onoff_t0    = out.area(a).onoff(in_area(a).T,:);      
+            in_area(a).Pthermal_t0 = out.area(a).Pthermal(in_area(a).T);    
+            [in_area(a).On_t0,in_area(a).Off_t0] = getOnoff_t0(out.area(a).onoff,in_area(a).Minup,in_area(a).Mindown);       
         end
-        in.area(a).Demand    = in_full.area(a).Demand(RollStart(roll):RollStart(roll+1)-1,:);        
-        in.area(a).Windmax   = in_full.area(a).Windmax(RollStart(roll):RollStart(roll+1)-1,:); 
-        in.area(a).PVmax     = in_full.area(a).PVmax(RollStart(roll):RollStart(roll+1)-1,:);  
-        in.area(a).ReserveUp = in_full.area(a).ReserveUp(RollStart(roll):RollStart(roll+1)-1,:);      %% up reserve
-        in.area(a).ReserveDn = in_full.area(a).ReserveDn(RollStart(roll):RollStart(roll+1)-1,:);      %% dowen reserve
+        in_area(a).Demand    = in_full.area(a).Demand(RollStart(roll):RollStart(roll+1)-1,:);        
+        in_area(a).Windmax   = in_full.area(a).Windmax(RollStart(roll):RollStart(roll+1)-1,:); 
+        in_area(a).PVmax     = in_full.area(a).PVmax(RollStart(roll):RollStart(roll+1)-1,:);  
+        in_area(a).ReserveUp = in_full.area(a).ReserveUp(RollStart(roll):RollStart(roll+1)-1,:);      %% up reserve
+        in_area(a).ReserveDn = in_full.area(a).ReserveDn(RollStart(roll):RollStart(roll+1)-1,:);      %% dowen reserve
     end
 
     %%solution
-    [out,hist]=multi_area_accommodation(in);
+    [out,hist]=multi_area_accommodation(in_area,A);
     %%results
     for a=1:A
         Pwind_F(RollStart(roll):RollStart(roll+1)-1,a)       = out.area(a).Pwind;    %% output of wind power 
@@ -78,44 +82,44 @@ delete(pool);
 %% save resluts
 save results.mat Pwind_F Ppv_F Pthermal_F onoff_F startup_F shutdown_F Ftie_F;
 %% display results
-Endh=24*32;
-for a=1:A
-    %%------------------------ thermal unit -------------------------------
-    figure;
-    stairs(Pthermal_F{a}(1:Endh,:));
-    title(['thermal unit output in area' num2str(a)]);
-    %%------------------------------- wind ------------------------------------
-    figure;
-    hold on;
-    stairs(in_full.area(a).Windmax(1:Endh));
-    stairs(Pwind_F(1:Endh,a));
-    title(['wind power in area' num2str(a)]);
-    legend('maximum','actual');
-    hold off;
-    %%-------------------------------- PV -------------------------------------
-    figure;
-    hold on;
-    stairs(in_full.area(a).PVmax(1:Endh));
-    stairs(Ppv_F(1:Endh,a));
-    title(['PV generation in area' num2str(a)]);
-    legend('maximum','actual');
-    hold off;  
-end
-%%------------------------------- tie lines -------------------------------
-for a=1:A
-    for b=a+1:A
-        for la=1:in_full.area(a).Ntie
-            for lb=1:in_full.area(b).Ntie
-                if (in_full.area(a).Tieline(la,1)==b)&&(in_full.area(b).Tieline(lb,1)==a)
-                    figure;
-                    hold on;
-                    stairs(Ftie_F{a}(1:Endh,la));
-                    stairs(Ftie_F{b}(1:Endh,lb));
-                    legend(['Tie ' num2str(a) '-' num2str(b)],['Tie ' num2str(b) '-' num2str(a)]); 
-                    title('tie line power flow');
-                    hold off;
-                end
-            end
-        end
-    end
-end
+% Endh=24*3*10;
+% for a=1:A
+%     %%------------------------ thermal unit -------------------------------
+%     figure;
+%     stairs(Pthermal_F{a}(1:Endh,:));
+%     title(['thermal unit output in area' num2str(a)]);
+%     %%------------------------------- wind ------------------------------------
+%     figure;
+%     hold on;
+%     stairs(in_full.area(a).Windmax(1:Endh));
+%     stairs(Pwind_F(1:Endh,a));
+%     title(['wind power in area' num2str(a)]);
+%     legend('maximum','actual');
+%     hold off;
+%     %%-------------------------------- PV -------------------------------------
+%     figure;
+%     hold on;
+%     stairs(in_full.area(a).PVmax(1:Endh));
+%     stairs(Ppv_F(1:Endh,a));
+%     title(['PV generation in area' num2str(a)]);
+%     legend('maximum','actual');
+%     hold off;  
+% end
+% %%------------------------------- tie lines -------------------------------
+% for a=1:A
+%     for b=a+1:A
+%         for la=1:in_full.area(a).Ntie
+%             for lb=1:in_full.area(b).Ntie
+%                 if (in_full.area(a).Tieline(la,1)==b)&&(in_full.area(b).Tieline(lb,1)==a)
+%                     figure;
+%                     hold on;
+%                     stairs(Ftie_F{a}(1:Endh,la));
+%                     stairs(Ftie_F{b}(1:Endh,lb));
+%                     legend(['Tie ' num2str(a) '-' num2str(b)],['Tie ' num2str(b) '-' num2str(a)]); 
+%                     title('tie line power flow');
+%                     hold off;
+%                 end
+%             end
+%         end
+%     end
+% end
